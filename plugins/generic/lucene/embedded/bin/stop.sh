a5f28842376e7396552f1a80847ae06c2f575dea
Ronan McHugh <mchugh.r@gmail.com>
2014-06-26 13:38:22 +0200

    do not return false when solr is not running

--- plugins/generic/lucene/embedded/bin/stop.sh
+++ plugins/generic/lucene/embedded/bin/stop.sh
@@ -6,7 +6,6 @@ source "$EXEC_PATH/script-startup"
 
 if [ ! -e $SOLR_PIDFILE ]; then
   echo "Solr PID-file not found. Is Solr stopped? Has the PID-file been deleted?"
-  exit 1
 fi
 
 # Stop the solr process.
@@ -16,5 +15,4 @@ if [ ! -z "$SOLR_PID" -a -e "/proc/$SOLR_PID" ]; then
   echo "Stopped solr."
 else
   echo "Solr not running."
-  exit 1
 fi
